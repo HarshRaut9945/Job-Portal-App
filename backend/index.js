accb5b9a3aff419aa1967dd43a0b2c2ffca225fc
HarshRaut9945 <harshraut9945@gmail.com>
2026-08-25 15:56:22 +0530

    backend work completed

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
 dotenv.config();
 
 const app = express();
@@ -23,6 +26,8 @@ app.use(cors(corsOptions));
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 app.get("/", (req, res) => {
     res.send("Job Portal API is running");
